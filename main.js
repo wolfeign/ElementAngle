eaecaf9db23ec4e85a7170dd4cc6c33761e5866f
wolfeign <28436131+wolfeign@users.noreply.github.com>
2023-06-08 20:09:27 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+﻿// 要素の角度取得関数(弧度および度数の2種)
+// 
+// 制作者:Wolfeign(@wolfeign)
+// 公開日:2023/06/08(Thu)
+// 更新日:---
+// ライセンス:MIT
+
+
+// 要素の角度を弧度法で得る
+// 引数:element=要素、win=通常はwindowオブジェクト(iframe内の要素はフレームのcontentWindow)
+// 返り値:弧度(失敗したらnull)
+function getElementAngleByRadian(element, win = window) {
+    if (!element || !win)
+        return null;
+
+    const matrix = win.getComputedStyle(element, null).transform;
+    const regx = /matrix\((.*)\)/i;
+
+    if (matrix && regx.test(matrix)) {
+        const values = matrix.match(regx)[1].replace(/\s*/g, "").split(",");
+
+        // scale値が必要な場合は以下を使用
+        // const scaleX = Math.hypot(parseFloat(values[0]), parseFloat(values[2]));
+        // const scaleY = Math.hypot(parseFloat(values[1]), parseFloat(values[3]));
+
+        const radian = Math.atan2(parseFloat(values[1]), parseFloat(values[3]));
+        if (radian < 0)
+            return 2 * 3.1415926535 + radian;
+
+        return radian;
+    }
+
+    return 0;
+}
+
+// 要素の角度を度数法で得る
+// 引数:element=要素、win=通常はwindowオブジェクト(iframe内の要素はフレームのcontentWindow)
+// 返り値:角度(0～360)
+function getElementAngleByDegree(element, win = window) {
+    if (!element || !win)
+        return null;
+
+    const matrix = win.getComputedStyle(element, null).transform;
+    const regx = /matrix\((.*)\)/i;
+
+    if (matrix && regx.test(matrix)) {
+        const values = matrix.match(regx)[1].replace(/\s*/g, "").split(",");
+        const radian = Math.atan2(parseFloat(values[1]), parseFloat(values[3]));
+
+        // 572957は、180/3.1415 の10000倍
+        const degree = parseFloat((radian * 572957 / 10000).toFixed(3));
+        if (degree < 0)
+            return 360 + degree;
+
+        return degree;
+    }
+
+    return 0;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < 5; i++) {
+        document.getElementById("transform" + i).textContent = document.getElementById("text" + i).style.transform;
+        document.getElementById("angle" + i).textContent = getElementAngleByDegree(document.getElementById("text" + i)) + "°";
+    }
+
+    document.getElementById("input").addEventListener("input", () => {
+        try {
+            document.getElementById("text").style.transform = document.getElementById("input").value;
+            document.getElementById("angle").textContent = getElementAngleByDegree(document.getElementById("text")) + "°";
+        } catch (e) {
+        }
+    }, false);
+}, false);
---
+++ readme.md
@@ -0,0 +1,43 @@
+# HTML Element Angle
+
+HTML要素の角度を求める関数。
+
+A function to find the angle of an HTML element.
+
+### デモ (Demo)
+
+https://wolfeign.github.io/ElementAngle/
+
+それぞれの角度は実行時に計算している。
+
+Each angle is calculated at runtime.
+
+### 使用方法 (Usage)
+
+関数は以下の2種
+・getElementAngleByRadian(element, win)
+・getElementAngleByDegree(element, win)
+
+引数のelementは要素、winは通常はwindowオブジェクトで省略可能。
+返り値は上が弧度で下が度数。エラーが起こればnullを返す。
+
+The following two types of functions.
+・getElementAngleByRadian(element, win)
+・getElementAngleByDegree(element, win)
+
+The argument element is an element and win is usually a window object and can be omitted.
+The return value is radians on the top and degrees on the bottom. Returns null if an error occurs.
+
+### 既知の問題 (Known Issues)
+
+今のところなし
+
+None for now.
+
+### 作者 (Author)
+
+Wolfeign(@wolfeign)
+
+### ライセンス (License)
+
+The MIT License (MIT)
